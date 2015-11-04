c17c218fa8b0fcb306e17c133441f46a3677fd90
alyannapauline24 <alyannapauline@gmail.com>
2015-11-04 14:41:28 +0800

    Initial commit

---
+++ createPOS.js
@@ -0,0 +1,98 @@
+var subjects  = [];
+
+(function init() {
+
+	$.get("subjectAddTemplate.html",  "", 
+	function(subjectTemplate){
+		$.template("subjectTemplate", subjectTemplate);	//for label of template	
+		
+		$.getJSON("subjects.json", function(data){
+				subjects = data;
+
+				for(var i=0; i<subjects.length; i++)
+				{
+					if(subjects[i].courseClassification == "MST"){
+						$.tmpl("subjectTemplate", subjects[i]).appendTo(".table.MST");
+					}
+					else if(subjects[i].courseClassification == "SSP"){
+						$.tmpl("subjectTemplate", subjects[i]).appendTo(".table.SSP");
+					}
+					
+					else {
+						$.tmpl("subjectTemplate", subjects[i]).appendTo(".table.AH");
+					}
+				}
+		});
+	}, 
+	"text");	
+	
+	updateHeading();
+	
+})();
+
+
+		function getCookie(cookieName){	
+		
+			var cookieArray = document.cookie.split(';');		//splits the string of cookies
+			
+			for(j=0; j<cookieArray.length; j++) 				//for checking all split string of cookies
+			{
+				var pairkey = cookieArray[j].split('=');		//splits each cookie to key and value
+				
+				if(pairkey[0] == cookieName){					//checks if key is equal to cookieName
+					return pairkey[1];								//returns the list of indexes
+				}
+			}		
+			return null;													//if not found
+		}
+
+
+		function updateHeading(){
+		
+			var listholder = getCookie("subjects");		//calls getCookie function, to find the list of indexes
+			var subjects;
+		
+			subjects = JSON.parse(listholder);
+			
+			var MSTcounter = 0;
+			var SSPcounter = 0;
+			var AHcounter = 0;
+			var updateHeading;
+			
+			if(subjects == null){				
+				updateHeading = $(".panel-heading.MST");			
+				updateHeading.text("MST (CURRENT NUMBER OF MST SUBJECTS: " + MSTcounter + ")");					
+				
+				updateHeading = $(".panel-heading.SSP");			
+				updateHeading.text("MST (CURRENT NUMBER OF SSP SUBJECTS: " + SSPcounter + ")");
+				
+				updateHeading = $(".panel-heading.AH");			
+				updateHeading.text("MST (CURRENT NUMBER OF MST SUBJECTS: " + AHcounter + ")");
+			
+				return				
+			}
+				
+			
+			
+			for(i = 0; i < subjects.length; i++){				
+				if(subjects[i].courseClassification == "MST"){
+					MSTcounter++;
+				}
+
+				else if(subjects[i].courseClassification == "SSP"){
+					SSPcounter++;
+				}				
+				
+				else	AHcounter++;				
+			}
+			
+			updateHeading = $(".panel-heading.MST");			
+			updateHeading.text("MST (CURRENT NUMBER OF MST SUBJECTS: " + MSTcounter + ")");					
+			
+			updateHeading = $(".panel-heading.SSP");			
+			updateHeading.text("MST (CURRENT NUMBER OF SSP SUBJECTS: " + SSPcounter + ")");
+			
+			updateHeading = $(".panel-heading.AH");			
+			updateHeading.text("MST (CURRENT NUMBER OF MST SUBJECTS: " + AHcounter + ")");
+			
+		}
---
+++ readme.txt
@@ -0,0 +1,7 @@
+index.html     -> main/home page
+createPOS.html -> where user can add subjects to POS, have createPOS.js for loading all possible subjects (subjects.json)
+checkPOS.html  -> where user can remove subects from POS, 
+
+
+subjectAddTemplate.html   -> template to use for createPOS
+subjectDeleteTemplate.html  -> template to use for deletePOS
